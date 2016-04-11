83fb65785bd1a9ef9b96b84fed0f9cbf6564da12
Raphael Sprumont <raphael.sprumont@opengeode.be>
2016-04-11 14:44:16 +0200

    deplacement

--- django-geode-geocoding/geode_geocoding/keys.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import settings
-
-bing_key = settings.BING_API_KEY
-google_key = settings.GOOGLE_API_KEY
-google_client = settings.GOOGLE_CLIENT
-google_client_secret = settings.GOOGLE_CLIENT_SECRET
