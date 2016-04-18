62ff4042d05aa2edfa8d91342ab995f3dabaf9fd
Raphael Sprumont <raphael.sprumont@opengeode.be>
2016-04-18 12:59:33 +0200

    rm url

--- geode_geocoding/urls.py
+++ geode_geocoding/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^test$', views.test, name='test'),
     url(r'^auto/$', views.AutocompleteAdresse, name='AutocompleteAdresse'),
 ]
